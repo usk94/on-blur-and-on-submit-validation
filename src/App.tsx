42318e035988f0d8fb837c8f30dc19bc8776e08d
Yusaku <hitoyasablue@gmail.com>
2023-09-22 20:03:42 +0900

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -25,35 +25,33 @@ const App = () => {
   const onSubmit = (data: FormType) => console.log(data)
 
   return (
-    <div className="w-screen h-screen">
-      <div className="flex flex-col justify-center items-center">
-        <form onSubmit={handleSubmit(onSubmit)} className="">
-          <p>名前</p>
-          <input {...register("name")} />
-          {errors.name?.message && <p className="text-red-500">{errors.name?.message}</p>}
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-screen h-screen justify-center items-center">
+      <p>名前</p>
+      <input {...register("name")} className="border border-gray-300 rounded" />
+      {errors.name?.message && <p className="mt-2 text-red-500">{errors.name?.message}</p>}
 
-          <p>性別</p>
-          <div className="flex flex-row">
-            {radioOptions.map((option) => {
-              const { label, value } = option
-              return (
-                <label key={value}>
-                  <input type="radio" value={value} {...register("gender")} />
-                  {label}
-                </label>
-              )
-            })}
-          </div>
-          {errors.gender?.message && <p className="text-red-500">{errors.gender?.message}</p>}
-
-          <p>メールアドレス</p>
-          <input {...register("email")} />
-          {errors.email?.message && <p className="text-red-500">{errors.email?.message}</p>}
-
-          <button type="submit">submit!</button>
-        </form>
+      <p className="mt-4">性別</p>
+      <div className="mt-2 flex flex-row">
+        {radioOptions.map((option) => {
+          const { label, value } = option
+          return (
+            <label key={value}>
+              <input type="radio" value={value} {...register("gender")} />
+              {label}
+            </label>
+          )
+        })}
       </div>
-    </div>
+      {errors.gender?.message && <p className="mt-2 text-red-500">{errors.gender?.message}</p>}
+
+      <p className="mt-4">メールアドレス</p>
+      <input {...register("email")} className="mt-2 border border-gray-300 rounded" />
+      {errors.email?.message && <p className="mt-2 text-red-500">{errors.email?.message}</p>}
+
+      <button type="submit" className="mt-4 border border-blue-500 rounded text-blue-500">
+        submit!
+      </button>
+    </form>
   )
 }
 
