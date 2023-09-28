a52bc81532ba0f0cac19f9e14ba81621a0c5d88a
Yusaku <hitoyasablue@gmail.com>
2023-09-28 10:13:59 +0900

    create card radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,79 @@
-import { useForm } from "react-hook-form"
-import { FormType, schema } from "./validator"
+import { useForm, useWatch } from "react-hook-form"
+import { FormType, PaymentMethodType, schema } from "./validator"
 import { zodResolver } from "@hookform/resolvers/zod"
 
-const radioOptions = [
-  { label: "男", value: "male" },
-  { label: "女", value: "female" },
-  { label: "その他", value: "neither" },
-]
+const paymentMethods = ["card", "paypal", "convini"] as const
 
 const App = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<FormType>({
     defaultValues: {
-      name: "",
-      gender: "",
-      email: "",
+      selectedPaymentMethod: "card",
+      card: {
+        number: "",
+        name: "",
+        expiryYear: "",
+        expiryMonth: "",
+      },
+      paypal: {
+        email: "",
+      },
+      convini: {
+        conviniName: null,
+      },
     },
     mode: "onBlur",
     resolver: zodResolver(schema),
   })
 
   const onSubmit = (data: FormType) => console.log(data)
+  const selectedPaymentMethod = useWatch({ control, name: "selectedPaymentMethod" })
+  const isActive = (paymentMethod: PaymentMethodType) => paymentMethod === selectedPaymentMethod
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-screen h-screen justify-center items-center">
+      <p className="text-xl font-semibold">支払い方法</p>
+
       <p>名前</p>
-      <input {...register("name")} className="border border-gray-300 rounded" />
-      {errors.name?.message && <p className="mt-2 text-red-500">{errors.name?.message}</p>}
+      <input {...register("paypal.email")} className="border border-gray-300 rounded" />
+      {errors?.paypal?.email?.message && <p className="mt-2 text-red-500">{errors?.paypal?.email?.message}</p>}
 
       <p className="mt-4">性別</p>
       <div className="mt-2 flex flex-row">
-        {radioOptions.map((option) => {
-          const { label, value } = option
-          return (
-            <label key={value}>
-              <input type="radio" value={value} {...register("gender")} />
-              {label}
-            </label>
-          )
+        {paymentMethods.map((method) => {
+          switch (method) {
+            case "card":
+              return isActive(method) ? (
+                <button type="button" className="w-full rounded-lg border  py-4 px-4 border-gray-200">
+                  <label>
+                    <input type="radio" value="card" {...register("selectedPaymentMethod")} />
+                    <div className="flex items-center gap-x-3">
+                      <div className="text-left">
+                        <p className="text-sm font-semibold text-black">カード</p>
+                      </div>
+                      {/* フォーム */}
+                    </div>
+                  </label>
+                </button>
+              ) : (
+                <button type="button" className="w-full rounded-lg border  py-4 px-4 border-gray-200">
+                  <label key={method}>
+                    <input type="radio" value={method} {...register("selectedPaymentMethod")} />
+                    <div className="flex items-center gap-x-3">
+                      <div className="text-left">
+                        <p className="text-sm font-semibold text-black">カード</p>
+                      </div>
+                    </div>
+                  </label>
+                </button>
+              )
+          }
         })}
       </div>
-      {errors.gender?.message && <p className="mt-2 text-red-500">{errors.gender?.message}</p>}
-
-      <p className="mt-4">メールアドレス</p>
-      <input {...register("email")} className="mt-2 border border-gray-300 rounded" />
-      {errors.email?.message && <p className="mt-2 text-red-500">{errors.email?.message}</p>}
 
       <button type="submit" className="mt-4 border border-blue-500 rounded text-blue-500">
         submit!
