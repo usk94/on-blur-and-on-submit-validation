e5189880ae54dfdfa19be838d152f0ceecf93840
Yusaku <hitoyasablue@gmail.com>
2023-10-02 09:55:06 +0900

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -34,42 +34,51 @@ const App = () => {
   const selectedPaymentMethod = useWatch({ control, name: "selectedPaymentMethod" })
   const isActive = (paymentMethod: PaymentMethodType) => paymentMethod === selectedPaymentMethod
 
+  console.log("here", selectedPaymentMethod)
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-screen h-screen justify-center items-center">
       <p className="text-xl font-semibold">支払い方法</p>
-
-      <p>名前</p>
-      <input {...register("paypal.email")} className="border border-gray-300 rounded" />
-      {errors?.paypal?.email?.message && <p className="mt-2 text-red-500">{errors?.paypal?.email?.message}</p>}
-
-      <p className="mt-4">性別</p>
-      <div className="mt-2 flex flex-row">
+      <div className="mt-2 flex flex-col gap-y-4 w-80">
         {paymentMethods.map((method) => {
           switch (method) {
             case "card":
-              return isActive(method) ? (
-                <button type="button" className="w-full rounded-lg border  py-4 px-4 border-gray-200">
-                  <label>
-                    <input type="radio" value="card" {...register("selectedPaymentMethod")} />
+              return (
+                <label className="flex w-full rounded-lg border py-4 px-4 border-gray-200">
+                  <input type="radio" value="card" {...register("selectedPaymentMethod")} />
+                  <div className="flex items-center gap-x-3">
+                    <div className="text-left">
+                      <p className="ml-1 text-sm font-semibold text-black">カード</p>
+                    </div>
+                  </div>
+                  {isActive(method) && (
+                    <div>
+                      <input {...register("card.number")} className="border border-gray-300 rounded ml-2" />
+                    </div>
+                  )}
+                </label>
+              )
+            case "paypal":
+              return (
+                <label className="flex flex-col border-gray-200 w-full rounded-lg border py-4 px-4">
+                  <div className="flex">
+                    <input type="radio" value="paypal" {...register("selectedPaymentMethod")} />
                     <div className="flex items-center gap-x-3">
                       <div className="text-left">
-                        <p className="text-sm font-semibold text-black">カード</p>
-                      </div>
-                      {/* フォーム */}
-                    </div>
-                  </label>
-                </button>
-              ) : (
-                <button type="button" className="w-full rounded-lg border  py-4 px-4 border-gray-200">
-                  <label key={method}>
-                    <input type="radio" value={method} {...register("selectedPaymentMethod")} />
-                    <div className="flex items-center gap-x-3">
-                      <div className="text-left">
-                        <p className="text-sm font-semibold text-black">カード</p>
+                        <p className="ml-1 text-sm font-semibold text-black">paypal</p>
                       </div>
                     </div>
-                  </label>
-                </button>
+                  </div>
+                  {isActive(method) && (
+                    <div className="flex mt-4 justify-center items-center">
+                      <p className="text-sm">メールアドレス</p>
+                      <input {...register("paypal.email")} className="border border-gray-300 rounded ml-2" />
+                    </div>
+                  )}
+                  {isActive(method) && errors?.paypal?.email?.message && (
+                    <p className="mt-2 text-red-500 text-sm">{errors?.paypal?.email?.message}</p>
+                  )}
+                </label>
               )
           }
         })}
